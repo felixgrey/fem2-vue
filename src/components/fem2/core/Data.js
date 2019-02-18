2766278bf026781317f0df02fa64e1abed6855e5
guo.feng <guo.feng@neusoft.com>
2019-02-18 15:15:54 +0800

    dataSet

--- src/components/fem2/core/Data.js
+++ src/components/fem2/core/Data.js
@@ -1,36 +1,22 @@
-
-const STOPRUN = Math.random() * 10e16;
-
-function monkey(fun, before) {
-  return function proxyFun(...args) {
-    let _args = args;
-    if (before) {
-      _args = before.bind(this)(args);
-      if(_args === STOPRUN) {
-        return args;
-      }
-    }
-
-    return fun.apply(this, _args);
-  }
-}
-
-function noValue(value){
+/*
+ 空值检查函数
+ */
+export function noValue(value) {
   return (value === null || value === undefined);
 }
 
 /*
- 预定义聚合函数
+ 预定义的聚合函数
  */
-const _AGGREGATES= {
-  'sum': (param = {}) => {
-    const {field, value, item, defaultValue} = param;  
+const _AGGREGATES = {
+  // 求和
+  'sum': ({field, value, item, defaultValue}) => { 
     const currentValue = noValue(value) ? defaultValue: value;
     const itemValue = noValue(item[field]) ? defaultValue : item[field]; 
     return currentValue + itemValue;
   },
-  'aver': (param = {}) => {
-    const {field, item, defaultValue, keyCounts, data} = param;
+  // 平均值
+  'aver': ({field, item, defaultValue, keyCounts, data}) => { 
     const itemValue = noValue(item[field]) ? defaultValue : item[field];    
     const tempValueKey = `_currentSumValue.${field}`;
     if (data[tempValueKey] === undefined) {
@@ -38,26 +24,24 @@ const _AGGREGATES= {
     }
     return (data[tempValueKey] += itemValue) / (keyCounts + 1);
   },
-  'max': (param = {}) => {
-    const {field, value, item, defaultValue} = param;   
+  // 最大值
+  'max': ({field, value, item, defaultValue}) => { 
     const currentValue = noValue(value) ? -Infinity: value;
     const itemValue = noValue(item[field]) ? defaultValue : item[field];  
     return currentValue > itemValue ? currentValue : itemValue;
   },
-  'min': (param = {}) => {
-    const {field, value, item, defaultValue} = param;   
+  // 最小值  
+  'min': ({field, value, item, defaultValue}) => { 
     const currentValue = noValue(value) ? Infinity: value;
     const itemValue = noValue(item[field]) ? defaultValue : item[field];
     return currentValue < itemValue ? currentValue : itemValue;
   },
+   // 聚合条数
   'count': ({keyCounts}) => keyCounts + 1,
-  'join': (param = {}) => {
-    const {field, value, item, option, keyCounts, defaultText} = param;
+   // 字符串连接
+  'join': ({field, value, item, option, keyCounts, defaultText}) => {
     const itemValue = noValue(item[field]) ? defaultText : item[field];
-    if (keyCounts === 0) {
-      return itemValue;
-    }
-    return `${value}${option.$split || ','}${itemValue}`;
+    return keyCounts === 0 ? itemValue : `${value}${option.$split || ','}${itemValue}`;
   }
 };
 
@@ -65,102 +49,75 @@ const AGGREGATES = {..._AGGREGATES};
 export {AGGREGATES};
 
 /*
-  一个值只写入一遍的list
-*/
-class _uniList {
-  constructor(){
-    this._list =[];
-    const _keyMap = this._keyMap = {};  
-    this._list.push = monkey(this._list.push, function(args){
-      const value = args[0];
-      if(_keyMap[value]){
-        return STOPRUN;     
-      }
-      _keyMap[value] = 1;
-      return args;
-    })
-    return this._list;
-  }
-}
-
-/*
-  按照KEY的插入顺序遍历的map
-*/
-export class OrderedMap {
-  constructor(name = null){
-    this.name = name;
-    this._map = {};
-    this._keyMap = {};
-    this._keyList = [];
+  树形数据结构，fieldName为分组字段值，为空表示是叶子节点
+ */
+export class DataMap extends Map {
+  constructor(initData, fieldName = null) {
+    super(initData);
+    this.fieldName = fieldName;
     this._keyCount = {};
   }
-
-  put(key, value){
-    if (!this._keyMap[key]) {
-      this._keyList.push(key);
-      this._keyMap[key] = 1;
+  
+  /*
+    set时记录同一个key被set的次数
+   */
+  set(key, value) {
+    if (this._keyCount[key] === undefined) {
       this._keyCount[key] = 0;
     }
     this._keyCount[key]++;
-    return this._map[key] = value;
-  }
-
-  get(key, blankMap = false ) {
-    if(noValue(this._map[key]) && blankMap){
-      return new OrderedMap();
-    }
-    return this._map[key];
+    super.set(key, value);
   }
   
-  trace(param = {_list:[], _item: {}}){
-    const {_list, _item } = param;
-     
-    if(this.name === null) {
-      this.keys().forEach(key => {
+  /*
+     取得同一个key被set的次数
+   */
+  keyCounts(key) {
+    return this._keyCount[key] || 0;
+  }
+  
+  /*
+     遍历数据并转为对象数组格式
+  */
+  toObjectInArray(_param = {_list: [], _item: {}}) {
+    const {_list, _item } = _param;   
+    const keys = Array.from(this.keys());  
+    
+    if (this.fieldName === null) {
+      keys.forEach(key => {
         _item[key] = this.get(key);
       });
       _list.push(_item);      
     } else {
-      this.keys().forEach(key => {
-        _item[this.name] = key;
-        param._item = {..._item};
-        this.get(key).trace(param);
+      keys.forEach(key => {
+        _item[this.fieldName] = key;
+        _param._item = {..._item};
+        (this.get(key) || new DataMap()).toObjectInArray(_param);
       });
-    }
+    } 
     
-    return param;
-  }
-
-  keyCounts(key) {
-    return this._keyCount[key] || 0;
-  }
-
-  keys() {
-    return this._keyList;
-  }
-
-  values() {
-    return this._keyList.map((key) => {
-        return this._map[key]
-    })
+    return _param._list;
   }
 }
 
-export class DataSet {
+/*
+  用于做聚合操作的数据集
+ */
+export class DataSetTransformer {
   constructor(config = {}) {
     this._init(config);
   }
   
-  _init(config){
+  _init(config) {
     const {
-      dataSource = [],
-      groupFields = [],
-      valueFields = [],
-      aggregate = {},
-      option = {
-        $defaultValue: 0,
-        $defaultText: '',
-        $split : ','
+      dataSource = [], // 数据源
+      groupFields = [], // 分组字段
+      valueFields = [], // 聚合字段
+      aggregate = {}, // 聚合函数
+      option = { // 额外配置
+        $defaultValue: 0, // 数值默认值
+        $defaultText: '', // 字符串默认值
+        $split : ',' // 字符串连接分隔符
       }
     } = config;
 
@@ -171,10 +128,10 @@ export class DataSet {
       _defaultValue: option.$defaultValue,
       _defaultText: option.$defaultText,
       _config: config,
-      _data: new OrderedMap(groupFields[0]),
+      _data: new DataMap(null, groupFields[0]),
       _option: option,
       _currentCount: 0,
-      _groupFieldValues: groupFields.map( () => (new _uniList()))
+      _groupFieldValues: groupFields.map(() => new Set())
     });
     
     this._aggregate = {};
@@ -190,20 +147,20 @@ export class DataSet {
     
     let currentData = this._data;
     this._groupFields.forEach((groupField, index) => {
-      if(!currentData.get(item[groupField])) {
-        currentData.put(item[groupField], new OrderedMap(this._groupFields[index + 1]));
+      if (!currentData.get(item[groupField])) {
+        currentData.set(item[groupField], new DataMap(null, this._groupFields[index + 1]));
       }
       currentData = currentData.get(item[groupField]);
-      this._groupFieldValues[index].push(item[groupField]);
+      this._groupFieldValues[index].add(item[groupField]);
     });
 
     this._valueFields.forEach((valueField) => {
       const aggregate = this._aggregate[valueField];
+      
       const currentValue = aggregate({
         field: valueField,
         value: currentData.get(valueField),
         item,
-        uniKey: aggregate.uniKey,
         keyCounts: currentData.keyCounts(valueField),
         option: this._option,
         count: this._currentCount, 
@@ -211,21 +168,55 @@ export class DataSet {
         defaultText: this._defaultText,
         data: currentData
       });
-      currentData.put(valueField, currentValue); 
+      
+      currentData.set(valueField, currentValue); 
     });    
   } 
 
-  output() {
-    
+  output() {  
     const fields = {};
-    this._groupFieldValues.forEach((a, i) => {
-      fields[this._groupFields[i]] = [].concat(a);
-    });
+    this._groupFieldValues.forEach((valueSet, index) => {
+      fields[this._groupFields[index]] = Array.from(valueSet) ;
+    }); 
     
     return {
       enums: fields,
-      list: this._data.trace()._list
+      list: this._data.toObjectInArray()
     };
   }
 }
 
+/*
+  数据集转换函数
+ */
+export function transform(config) {
+  return new DataSetTransformer(config).output();
+}
+transform.AGGREGATES = AGGREGATES;
+
+/*
+ ArrayInArray to ObjectInArray
+ */
+transform.fromArrayInArray = function(dataSource) {
+  dataSource = [].concat(dataSource);
+  const fields = dataSource.shift() || {};
+  return dataSource.map(item => {
+    const value = {};
+    fields.forEach((field, index) => {
+      value[field] = item[index];
+    });
+    return value;
+  });
+}
+
+/*
+ ObjectInArray to ArrayInArray
+ */
+transform.fromObjectInArray = function(dataSource) {
+  const keys = Object.keys(dataSource[0] || {});
+  const newSource = dataSource.map(item => {
+    return keys.map(key => item[key]);
+  });
+  newSource.splice(0, 0, keys);
+  return newSource;
+}
--- src/components/helloData.js
+++ src/components/helloData.js
@@ -1,23 +1,36 @@
 /* eslint-disable */
-import {DataSet, AGGREGATES} from '@/components/fem2';
+import {transform} from '@/components/fem2';
 
+const {
+  AGGREGATES,  // 自带的聚合函数
+  fromArrayInArray, // 二维数组：ArrayInArray（第一条是字段名数组）格式转为对象数组：ObjectInArray格式
+  fromObjectInArray // 对象数组：ObjectInArray格式转为二维数组：ArrayInArray（第一条是字段名数组）格式
+} = transform;
 
+// 自带的全部6个聚合函数
+const {
+  sum, // 求和
+  aver, // 平均值
+  count, // 聚合条数
+  join, // 字符串连接
+  max, // 最大值
+  min // 最小值
+} = AGGREGATES;
+
+// 测试数据
 const data = [];
-
-for (let i = 0 ;i < 8 ;i++) {
+for (let i = 0 ; i < 8; i++) {
   data.push({
-    id:i + 1,
-    hospital: i%3 !=0 ? '张三医院' :'李四医院',
+    id:i+1,
+    hospital: i%3 == 0 ? '张三医院' : '李四医院',
     sex: i%2 ? '男' : '女',
     money: i*100 +5,
-    age: 10+2*i,
-    
+    age: 10+2*i,  
   });
 }
-
-for (let i = 8 ;i < 12 ;i++) {
+for (let i = 8; i < 12; i++) {
   data.push({
-    id: i + 1,
+    id: i+1,
     hospital: '王五医院',
     sex: i%3 ? '女' : '男',
     money: i*200,
@@ -25,27 +38,120 @@ for (let i = 8 ;i < 12 ;i++) {
   });
 }
 
-console.log(data); // 原始数据
+// 原始数据
+console.log(data); 
 
-console.log(new DataSet({
-  dataSource: data, // 数据源
-  aggregate: { // 字段聚合函数
-    money: AGGREGATES.sum, // 求和
-    age: AGGREGATES.aver, // 平均数
-    count: AGGREGATES.count, // 总数
-    id: AGGREGATES.join, // 连接
-    maxMoney: (param) => { // 计算字段
-      param.field = 'money'; // 原字段
-      return AGGREGATES.max(param); // 最大值
+// 聚合后的数据
+const transformedData = transform({
+  dataSource: data, // 数据源，需要ObjectInArray格式
+  aggregate: { // 配置字段聚合函数
+//  money: sum, // 默认求和
+    age: aver, // 平均年龄
+    count: count, // 自定义字段：聚合条数
+    id: join, // 聚合的数据id
+    maxMoney: param => { // 用自定义的聚合函数计算的自定义字段：最高金额
+      param.field = 'money'; // 用于计算的字段
+      return max(param);
     },
-    minAge: (param) => { // 计算字段
-      param.field = 'age'; // 原字段
-      return AGGREGATES.min(param); // 最小值
+    minAge: param => {
+      param.field = 'age'; 
+      return min(param);
     },
   },
-  groupFields:['hospital','sex'],
-//groupFields:['hospital'],
-//groupFields:[],
-  valueFields:['money','age','id','count','maxMoney','minAge'],
-//valueFields:[]
-}).output());
+  option: { // 额外配置
+    $defaultValue: 0, // 数值型字段值为空时的默认值, 默认0（sum、aver、max、min函数使用）
+    $defaultText: '', // 字符串型字段值为空时的默认值，默认 ''（join函数使用）
+    $split : ',' // 字符串连接聚合时的分隔符,默认 ','（join函数使用）
+  },
+  groupFields: ['hospital', 'sex'], // 所有分组字段
+//groupFields: ['hospital'],
+//groupFields: [],
+  valueFields: ['money', 'age', 'id', 'count', 'maxMoney', 'minAge'], // 所有聚合字段，包括自定义的字段count、maxMoney、minAge
+//valueFields: []
+});
+
+console.log(transformedData);
+
+// 两种数据集格式转换
+const arrayInArray = fromObjectInArray(transformedData.list);
+console.log(arrayInArray);
+console.log(fromArrayInArray(arrayInArray));
+
+/* 
+ transformedData =
+  {
+    "enums": { // 分组后的字段值枚举
+      "hospital": [ // 按照插入顺序
+        "张三医院", 
+        "李四医院",
+        "王五医院"
+      ],
+      "sex": [
+        "女",
+        "男"
+      ]
+    },
+    "list": [ // 聚合后的数据列表
+      {
+        "hospital": "张三医院",
+        "sex": "女",
+        "money": 610,
+        "age": 16,
+        "id": "1,7",
+        "count": 2,
+        "maxMoney": 605,
+        "minAge": 10
+      },
+      {
+        "hospital": "张三医院",
+        "sex": "男",
+        "money": 305,
+        "age": 16,
+        "id": 4,
+        "count": 1,
+        "maxMoney": 305,
+        "minAge": 16
+      },
+      {
+        "hospital": "李四医院",
+        "sex": "男",
+        "money": 1315,
+        "age": 18.666666666666668,
+        "id": "2,6,8",
+        "count": 3,
+        "maxMoney": 705,
+        "minAge": 12
+      },
+      {
+        "hospital": "李四医院",
+        "sex": "女",
+        "money": 610,
+        "age": 16,
+        "id": "3,5",
+        "count": 2,
+        "maxMoney": 405,
+        "minAge": 14
+      },
+      {
+        "hospital": "王五医院",
+        "sex": "女",
+        "money": 5800,
+        "age": 39.333333333333336,
+        "id": "9,11,12",
+        "count": 3,
+        "maxMoney": 2200,
+        "minAge": 36
+      },
+      {
+        "hospital": "王五医院",
+        "sex": "男",
+        "money": 1800,
+        "age": 38,
+        "id": 10,
+        "count": 1,
+        "maxMoney": 1800,
+        "minAge": 38
+      }
+    ]
+  }
+*/
