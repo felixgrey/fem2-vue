6fde87a2397870d6e23e70281ac2bc46ad5fa2e6
guo.feng <guo.feng@neusoft.com>
2019-02-26 14:48:02 +0800

    radar

--- src/components/common/fem2DataVisualization/components/echarts/index.js
+++ src/components/common/fem2DataVisualization/components/echarts/index.js
@@ -1,9 +1,9 @@
 export * from './Utils.js'; // 工具
-export * from './lba.js';// 折线 条 区域
-export * from './sh.js';// 散点 热力
-export * from './rp.js';// 雷达 饼
-export * from './cloud.js';// 词云
-export * from './wave.js';// 水波
+export * from './lba.js';// 折线图 条形图 区域图
+export * from './sh.js';// 散点图 热力图
+export * from './rp.js';// 雷达图 饼图
+export * from './cloud.js';// 词云图
+export * from './wave.js';// 水波图
 export * from './sg.js';// 桑基图 关系图
-export * from './ts.js'; // 树图 矩形树图 旭日图
+export * from './ts.js'; // 树图 旭日图
 export * from './bmap.js'; // 百度地图
