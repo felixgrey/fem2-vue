07a58ac2cdadb32cd8f9151b21edd7ee62e8a6c5
guo.feng <guo.feng@neusoft.com>
2019-02-28 11:52:29 +0800

    rebuild

--- src/components/business/Index.vue
+++ src/components/business/Index.vue
@@ -6,7 +6,7 @@
     ========================================================================
     <HelloEchartsRadar />
     <HelloEchartsPie />
-    <HelloEchartsLba/>
+    <HelloEchartsLba />
     <HelloEchartsBmap />   
   </div>
 </template>
@@ -15,16 +15,30 @@
 /* eslint-disable */
   import menu from './config/menu.js';
   
+  function noValue(value) {
+    return (value === null || value === undefined);
+  }
+  
   const components = {};
+  const menuLinks = [];
+ 
   menu.forEach(item => {
+    // 组件
     const {name, component} = item;
-    components[name] = component;
+    (!noValue(name)) && component && (components[name] = component);
+    
+    // 路由
+    const {path = name, redirect} = item;
+    
+    // 菜单
+    const {text = name, icon, detail, hidden, hiddenChildren, disable} = item;
   });
 
   export default {
-    name: 'layout',
+    name: 'index',
     data:function(){
       return {
+        menuLinks
       };
     },
     components
